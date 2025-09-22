3505edb0a75667339bd26de926f151c6c8e9c445
Belal Salah <belal.salah259@gmail.com>
2025-09-22 20:30:13 +0300

    chore: create the post interface and integrate it in the service

--- src/app/core/services/post.ts
+++ src/app/core/services/post.ts
@@ -2,17 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment.development';
-
+import { Post } from '../../shared/models/post.model';
 @Injectable({
   providedIn: 'root'
 })
-export class Post {
+export class PostService {
 
   private readonly Http = inject(HttpClient);
 
-  getPostById(): Observable<unknown> {
-    return this.Http.get<unknown>(environment.apiUrl + '/posts/1');
+  getPostById(): Observable<Post> {
+    return this.Http.get<Post>(environment.apiUrl + '/posts/1');
   }
-
-
 }
